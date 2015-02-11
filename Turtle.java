cde20357aa2fa4366cd5b7c3f4268e5a409ce84c
thebareju <noahbweiner@gmail.com>
2015-02-11 11:57:19 -0800

    implement koch snowflake

--- Turtle.java
+++ Turtle.java
@@ -12,12 +12,12 @@ public class Turtle extends JPanel {
   }
   ArrayList<Line> lines;
 
-  public Turtle(int width, int height) {
+  public Turtle(int width, int height, int x1, int y1) {
     setPreferredSize(new Dimension(width, height));
 
     lines = new ArrayList<Line>();
-    x = (double)width / 2;
-    y = (double)height / 2;
+    x = x1;
+    y = y1;
     angle = 0.0;
   }
 
@@ -26,13 +26,10 @@ public class Turtle extends JPanel {
 
     for (Line l : lines) {
       g.drawLine((int)l.x1, (int)l.y1, (int)l.x2, (int)l.y2);
-
-      System.out.println("Drawing line: " + l.x1 + ", " + l.y1 +
-        " to " + l.x2 + ", " + l.y2);
     }
   }
 
-  private void turnLeft(double delta) {
+  public void turnLeft(double delta) {
     angle += delta;
 
     if (angle > 2 * Math.PI) {
@@ -40,7 +37,7 @@ public class Turtle extends JPanel {
     }
   }
 
-  private void goForward(double step) {
+  public void goForward(double step) {
     Line l = new Line();
     l.x1 = x;
     l.y1 = y;
@@ -52,28 +49,5 @@ public class Turtle extends JPanel {
     l.y2 = y;
 
     lines.add(l);
-  }
-
-  public void test() {
-    double step = 200;
-    double decay = 0.9;
-
-    while (step > 10) {
-      goForward(step);
-      turnLeft(Math.PI / 3);
-      step *= decay;
-    }
-  }
-
-  public static void main(String[] args) {
-    JFrame frame = new JFrame();
-    Turtle t = new Turtle(500, 500);
-
-    t.test();
-
-    frame.getContentPane().add(t);
-    frame.pack();
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
 }
